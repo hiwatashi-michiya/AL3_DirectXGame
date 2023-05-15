4b0c641b2f305b7583858c405a8e5a6f2748cbd9
k022g0063@g.neec.ac.jp <k022g0063@g.neec.ac.jp>
2023-05-15 15:00:47 +0900

    02_09_ex1

--- EnemyBullet.cpp
+++ EnemyBullet.cpp
@@ -15,6 +15,16 @@ void EnemyBullet::Initialize(Model* model, const Vector3& position, const Vector
 	worldTransform_.Initialize();
 	worldTransform_.translation_ = position;
 	velocity_ = velocity;
+	worldTransform_.scale_.x = 0.5f;
+	worldTransform_.scale_.y = 0.5f;
+	worldTransform_.scale_.z = 3.0f;
+	//Y軸周り角度
+	worldTransform_.rotation_.y = float(std::atan2(double(velocity_.x), double(velocity_.z)));
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(-worldTransform_.rotation_.y);
+	Vector3 velocityZ = TransformNormal(velocity_, rotateYMatrix);
+	//X軸周り角度
+	worldTransform_.rotation_.x = float(std::atan2(double(-velocityZ.y), double(velocityZ.z)));
+
 
 }
 
